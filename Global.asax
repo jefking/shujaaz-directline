fabc0ac75f7c98b381316daea3aa2b748be70cb7
Jef King <jefkin@microsoft.com>
2016-10-27 13:36:03 +0300

    init

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DirectLineBot.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DirectLineBot.Startup))]
+namespace DirectLineBot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
